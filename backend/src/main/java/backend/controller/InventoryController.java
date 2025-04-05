5297b6ae615d770a9fe7e9f5feebe5a1d5c3bc0e
Tharusha Liyanage <147168826+Tharusha629@users.noreply.github.com>
2025-04-05 14:19:54 +0530

    Create Inventory Repository

--- backend/src/main/java/backend/controller/InventoryController.java
+++ backend/src/main/java/backend/controller/InventoryController.java
@@ -1,11 +1,21 @@
 package backend.controller;
 
+import backend.model.InventoryModel;
+import backend.repository.InventoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin("http://localhost:3000")
 public class InventoryController {
     @Autowired
+    private InventoryRepository inventoryRepository;
+
+    @PostMapping("/inventory")
+    public InventoryModel newInventoryModel(@RequestBody InventoryModel newInventoryModel){
+        return inventoryRepository.save(newInventoryModel);
+    }
 }
