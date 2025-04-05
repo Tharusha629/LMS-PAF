9682de8bd50cedef6768bac7e47ae215f1ea37e2
Tharusha Liyanage <147168826+Tharusha629@users.noreply.github.com>
2025-04-05 10:04:26 +0530

    Create Inventory Controller

---
+++ backend/src/main/java/backend/controller/InventoryController.java
@@ -0,0 +1,11 @@
+package backend.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+public class InventoryController {
+    @Autowired
+}
