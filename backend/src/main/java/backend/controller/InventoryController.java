5912a619158bf658eac1447373e8a94486c68af7
Tharusha Liyanage <147168826+Tharusha629@users.noreply.github.com>
2025-04-05 15:07:38 +0530

    Not found advice and exception

--- backend/src/main/java/backend/controller/InventoryController.java
+++ backend/src/main/java/backend/controller/InventoryController.java
@@ -3,10 +3,12 @@ package backend.controller;
 import backend.model.InventoryModel;
 import backend.repository.InventoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
 
 @RestController
 @CrossOrigin("http://localhost:3000")
@@ -17,5 +19,23 @@ public class InventoryController {
     @PostMapping("/inventory")
     public InventoryModel newInventoryModel(@RequestBody InventoryModel newInventoryModel){
         return inventoryRepository.save(newInventoryModel);
+    }
+
+    @PostMapping("/inventory/itemImg")
+    public String itemImage(@RequestParam("file") MultipartFile file){
+        String folder = "src/main/uploads/";
+        String itemImage = file.getOriginalFilename();
+
+        try{
+            File uploadDir = new File(folder);
+            if(!uploadDir.exists()){
+                uploadDir.mkdir();
+            }
+            file.transferTo(Paths.get(folder+itemImage));
+        }catch (IOException e){
+            e.printStackTrace();
+            return "Error uploading files;" +itemImage;
+        }
+        return itemImage;
     }
 }
