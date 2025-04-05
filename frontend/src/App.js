4e1e8e80b61c0c790d8ce469c86e5c460d6435ba
Tharusha Liyanage <147168826+Tharusha629@users.noreply.github.com>
2025-04-05 17:42:21 +0530

    All

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Routes } from "react-router"
+import Home from "./components/Home/Home";
+import AddItem from "./components/AddItem/AddItem";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <React.Fragment>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/additem" element={<AddItem />}/>
+        </Routes>
+      </React.Fragment>
+      
     </div>
   );
 }
